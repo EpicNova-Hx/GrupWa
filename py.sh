788f2784ebc53e94d43c4ef6f80bbb49bbf8ebe0
EpicNova-Hx <66078147+EpicNova-Hx@users.noreply.github.com>
2020-06-12 17:29:53 +0700

    Add files via upload

---
+++ py.sh
@@ -0,0 +1,88 @@
+#!/bin/sh
+#Halo Kang Recode
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+echo "Haloo"
+sleep 1
+echo "Siapa Nama kk [?] : "
+read -p "[?] Tuliskan Nama kk : " nama :
+#Batas Nama
+if [ $nama = $nama ]
+then
+echo $pur "Elite"$ku"V1"
+echo $cy"-----------------"$pur"-------------------"
+echo $cy" Author : Manca"
+echo $cy" Youtube : EpicNo"$pur"va"
+echo $cy" Whatsapp : 08880"$pur"2118339"
+echo $cy"-----------------"$pur"-------------------" 
+echo $cy"Selamat Datang,"$pur"$nama"
+echo $bi "Pilih" $i "Yang" $cy "Mana" $pu "[?]"
+sleep 2
+echo $ku"[1]Chat Wa Admin "
+echo $ku"[2]Clear "
+echo $ku"[3]Join Grup Squad Team "
+echo $ku"[4]Join Grup DeepWeb"
+echo $ku"[5]Join Grup Meme Indonesia"
+echo $ku"[6]Join Grup Muslim Cyber Army Team"
+echo $ku"[7]Join Grup The Nay Indonesia"
+read -p "[?]Choice : " pil :
+#Batas Pilih 1
+fi
+if [ $pil = 1 ]
+then
+xdg-open https://wa.me/6288802118339
+sleep 2
+sh py.sh
+fi
+#Batas Pilih 2
+if [ $pil = 2 ]
+then
+clear
+sleep 1
+figlet "CLEARED"
+sleep 2
+echo $cy "Jangan Lupa Support Dengan Cara Subscribe Channel EpicNova"
+fi
+#Batas Pilih 3
+if [ $pil = 3 ]
+then
+xdg-open https://chat.whatsapp.com/LjZkQynilNbAcGxBqViFvm
+sleep 1
+sh py.sh
+fi
+#Batas Pilih 4
+if [ $pil = 4 ]
+then
+xdg-open https://chat.whatsapp.com/GuHyLXbt71JIJjMGGGMl2y
+sleep 1
+sh py.sh
+fi
+#Batas Pilih 5
+if [ $pil = 5 ]
+then
+xdg-open https://chat.whatsapp.com/BpTw3p5gp29BkSDV9OWBuW
+sleep 1
+sh py.sh
+fi
+#Batas Pilih 6
+if [ $pil = 6 ]
+then
+xdg-open https://chat.whatsapp.com/IblV3KjnH6GLFK52LwTjgT
+sleep 1
+sh py.sh
+fi
+#Batas Pilih 7
+if [ $pil = 7 ]
+then
+xdg-open https://chat.whatsapp.com/LGjQUrjwR6LAhFpf5DbYme
+sleep 1
+sh py.sh
+fi
+#Halo Kang Recode
+#Lanjutannya Tersedia Di V2 Cok
